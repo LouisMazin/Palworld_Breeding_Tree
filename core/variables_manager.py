25dbcdc8b5ecd849def38646a1ce5a317b00a697
Louis Mazin <mazinlouis79@gmail.com>
2025-02-02 18:49:11 +0100

    fix

--- core/variables_manager.py
+++ core/variables_manager.py
@@ -39,7 +39,7 @@ class VariablesManager:
             "locked": []  # Ajouter le paramètre par défaut pour "combos"
         }
         self.load_config()
-        self.version = "2.0.0"
+        self.version = "3.0.0"
         self.language_manager = LanguageManager()
         if(self.config.keys() != self.defaults.keys()):
             self.config = self.defaults.copy()
