b2cc914a656b21a2246f4de414c0c81adda3af4e
Louis Mazin <mazinlouis79@gmail.com>
2025-02-09 02:15:06 +0100

    fix 2 combo

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@ pyinstaller --onefile --clean ^
 --noconsole ^
 --distpath "../Build" ^
 --specpath "../.Building" ^
---name "Palworld Breeding Tree - 3.0.0 - Windows" ^
+--name "Palworld Breeding Tree - 3.0.1 - Windows" ^
 --icon "../Source/Icons/icon.ico" ^
 --workpath "../.Building" ^
 --collect-all graphviz ^
